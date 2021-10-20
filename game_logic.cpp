969e5e1d37687101beca1d7e9f70772e17f9387c
qasy <alekseyivanov0705@gmail.com>
2021-10-20 23:57:15 +0300

    Added CMakeLists.txt but it needs to be fixed, and also filled the game-logic files a bit

---
+++ game_logic.cpp
@@ -0,0 +1,10 @@
+// Waiting for the player's actions
+void getUserInput()
+{
+    checkUserInput();
+}
+
+// Checking if user input is correct
+bool checkUserInput()
+{
+}
---
+++ game_logic.hpp
@@ -0,0 +1,5 @@
+// Waiting for the player's actions
+void getUserInput();
+
+// Checking if user input is correct
+bool checkUserInput();
