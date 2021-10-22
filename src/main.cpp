04c3d4a1cd1898d37d42e200fd021499235bfa2b
qasy <alekseyivanov0705@gmail.com>
2021-10-22 10:24:22 +0300

    Fixed tasks.json and launch.json to launch one key at a time

--- src/main.cpp
+++ src/main.cpp
@@ -7,48 +7,46 @@
 // If the game is running
 bool isPlaying = false;
 
-short logicFramePerSecond   = 10;
+short logicFramePerSecond = 10;
 short logicFrameDuration_ms = 1000 / logicFramePerSecond; // 1'000ms in 1s
-short renderFramePerSecond  = 25;
+short renderFramePerSecond = 25;
 
-int main()
-{
-    using namespace std::this_thread;     // sleep_for, sleep_until
-    using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+int main() {
+  using namespace std::this_thread;     // sleep_for, sleep_until
+  using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 
-    Player player;
+  Player player;
 
-    // Set up size of map
-    short height{15};
-    short width{15};
+  // Set up size of map
+  short height{15};
+  short width{15};
 
-    // Create base of the map for all levels
-    short area[height][width]{0};
+  // Create base of the map for all levels
+  short area[height][width]{0};
 
-    createMap(&area[0][0], height, width);
-    // initPlayer(&area[0][0], height, width, &player);
-    // add npc
+  createMap(&area[0][0], height, width);
+  // initPlayer(&area[0][0], height, width, &player);
+  // add npc
 
-    isPlaying      = true;
-    int frameCount = 0;
-    while (isPlaying && frameCount < 1000)
-    {
-        // Logic
-        //----
-        getUserInput();
+  isPlaying = true;
+  int frameCount = 0;
+  while (isPlaying && frameCount < 1000) {
+    // Logic
+    //----
+    getUserInput();
 
-        playerAction();
-        playerInit(&area[0][0], height, width, &player);
+    playerAction();
+    playerInit(&area[0][0], height, width, &player);
 
-        // Render
-        std::system("clear");
-        renderWorld(&area[0][0], height, width, &player);
-        //----
+    // Render
+    std::system("clear");
+    renderWorld(&area[0][0], height, width, &player);
+    //----
 
-        ++frameCount;
-        // Slow down the logic to the specified number of frames
-        sleep_for(logicFrameDuration_ms * 1ms);
-    }
+    ++frameCount;
+    // Slow down the logic to the specified number of frames
+    sleep_for(logicFrameDuration_ms * 1ms);
+  }
 
-    return 0;
+  return 0;
 }
